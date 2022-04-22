7a75fe667ac702e20f458554058075aa2e6bfadd
kaskiv <kaskiv11gmail.com>
2022-04-22 11:00:23 +0300

    Add project files.

---
+++ UnitTestLab5.3A/UnitTestLab5.3A.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.3A/Lab5.3A.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab53A
+{
+	TEST_CLASS(UnitTestLab53A)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char g[6] = "g.dat";
+			int t = binSearch(g, "1", 1, 1);
+			Assert::AreEqual(t, -1);
+		}
+	};
+}
